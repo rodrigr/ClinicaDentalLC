31e2eb3ba6c523ae12af739a740f0d335a20d4e3
rodrigr <rj.garcia.ribeiro@gmail.com>
2020-08-24 17:59:27 -0300

    blog added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
-let links = $('.nav-link')
+$(window).scroll(function(){
 
-links.on('click', function(){
+    if(window.innerWidth > 768){
+        if(this.scrollY > 100){
+            $('.top-area').attr('style','display:none!important;')
+        }else{
+            $('.top-area').attr('style','display:block!important;')
+        }
+    }
+    
+    
+})
+
+let nav_links = $('.nav-link')
+
+nav_links.on('click', function(){
     $('#navbarSupportedContent').removeClass('show')
+})
+
+let service_links = $('.service-link')
+
+service_links.on('click',function(){
+    $(this).children('.fa-angle-down').toggle()
+    $(this).children('.fa-angle-up').toggle()
 })
